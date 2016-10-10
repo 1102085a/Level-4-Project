c273e6a70f62e053ef87c2d450f586bdc3db6e28
1102085a <1102085A@student.gla.ac.uk>
2016-10-10 16:48:50 +0100

    Django initial project files set up

---
+++ projectallocationapp/projectallocationapp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for projectallocationapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectallocationapp.settings")
+
+application = get_wsgi_application()
